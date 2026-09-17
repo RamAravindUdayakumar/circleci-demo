9851511b2d7bb829091fb056f74bddacd7bf3c64
RamAravindUdayakumar <rampraga24@gmail.com>
2026-09-17 20:46:27 +0530

    14th commit

--- build/deploy_code.sh
+++ build/deploy_code.sh
@@ -1,5 +1,6 @@
-echo "Coverting source to metadata format"
-sfdx force:source:convert -d deploy_code -r force-app
-
 echo "Deploying code to org"
-sfdx force:mdapi:deploy -u DevHub -d deploy_code/ -w -1 -l RunLocalTests
+sf project deploy start \
+  --source-dir force-app \
+  --target-org DevHub \
+  --test-level RunLocalTests \
+  --wait 30
