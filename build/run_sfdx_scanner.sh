9e4d4f000a8beb36bfcc307de4c8e7ae5578d830
RamAravindUdayakumar <rampraga24@gmail.com>
2026-09-17 18:17:36 +0530

    7th commit

--- build/run_sfdx_scanner.sh
+++ build/run_sfdx_scanner.sh
@@ -1,9 +1,9 @@
 echo "Installing JDK"
-sudo apt-get update
-sudo apt-get install -y openjdk-11-jdk
+sudo apt-get update --allow-releaseinfo-change || sudo apt-get update
+sudo apt-get install -y --fix-missing openjdk-11-jdk
 
 echo "Install SFDX Scanner"
 echo 'y' | sfdx plugins:install @salesforce/sfdx-scanner
 
 echo "Running SFDX Scanner"
-npx sfdx scanner:run --target "**/default/**" --format "csv" --outfile "sfdxScannerAnalysis.csv" --violations-cause-error
+sfdx scanner:run --target "**/default/**" --format "csv" --outfile "sfdxScannerAnalysis.csv" --violations-cause-error
