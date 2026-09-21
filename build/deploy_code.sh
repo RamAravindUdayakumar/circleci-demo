0677f387e90ca6192477d9a8f8932b01bbc1be27
RamAravindUdayakumar <rampraga24@gmail.com>
2026-09-21 10:43:38 +0530

    16th commit

--- build/deploy_code.sh
+++ build/deploy_code.sh
@@ -2,5 +2,5 @@ echo "Deploying code to org"
 sf project deploy start \
   --source-dir force-app \
   --target-org DevHub \
-  --test-level RunSpecifiedTests --tests "AccpunthandlerTest" \
+  --test-level RunSpecifiedTests --tests "AccounthandlerTest" \
   --wait 30
